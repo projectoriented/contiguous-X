6c98fc0cf4b86cf1d765a8f6d8ce4666622ee050
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2023-10-11 16:03:38 -0700

    wip: okay some more refactoring

--- workflow/scripts/plot-ava.R
+++ workflow/scripts/plot-ava.R
@@ -1,22 +1,27 @@
 #!/usr/bin/env Rscript
+# Author: Mei Wu, https://github.com/projectoriented
 
-# # Load command line arguments
-# args <- commandArgs(trailingOnly = TRUE)
-#
-# # Check that arguments were provided
-# if (length(args) == 0) {
-#   stop("Error: No arguments provided.")
-# }
+# Logging
+log <- file(snakemake@log[[1]], open = "wt")
+sink(log)
+sink(log, type = "message")
 
 # Extract arguments
 paf.file.name <- snakemake@input[["oriented_ava_paf"]]
 out.pdf.name <- snakemake@output[["ava_pdf"]]
+seqnames.order <- snakemake@params[["desired_order"]]
 
 # Load the library
 library(SVbyEye)
 
+print(seqnames.order)
+
 paf.table <- readPaf(paf.file = paf.file.name, include.paf.tags = TRUE, restrict.paf.tags = "cg")
 
 pdf(out.pdf.name)
-plotAVA(paf.table = paf.table, color.by = "direction", min.deletion.size = 50, min.insertion.size = 50, highlight.sv = 'outline')
+plotAVA(
+    paf.table = paf.table, color.by = "direction",
+    min.deletion.size = 50, min.insertion.size = 50,
+    highlight.sv = 'outline', seqnames.order = seqnames.order
+)
 dev.off()
