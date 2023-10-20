3fd9051b97202e9bcbd23b16c006b7f3e2ca3fde
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2023-10-20 16:13:32 -0700

    wip: :snake:

--- workflow/scripts/plot-ava.R
+++ workflow/scripts/plot-ava.R
@@ -8,20 +8,27 @@ sink(log, type = "message")
 
 # Extract arguments
 paf.file.name <- snakemake@input[["oriented_ava_paf"]]
-out.pdf.name <- snakemake@output[["ava_pdf"]]
+out.png.name <- snakemake@output[["ava_png"]]
 seqnames.order <- snakemake@params[["desired_order"]]
 
 # Load the library
 library(SVbyEye)
+library(ggplot2)
 
 print(seqnames.order)
 
 paf.table <- readPaf(paf.file = paf.file.name, include.paf.tags = TRUE, restrict.paf.tags = "cg")
 
-pdf(out.pdf.name)
-plotAVA(
-    paf.table = paf.table, color.by = "direction",
-    min.deletion.size = 50, min.insertion.size = 50,
-    highlight.sv = 'outline', seqnames.order = seqnames.order
-)
+plt <- plotAVA(
+    paf.table = paf.table, color.by = 'direction',
+    min.deletion.size = 10000, min.insertion.size = 10000,
+    highlight.sv = 'outline', binsize = 100000,
+    seqnames.order = seqnames.order,
+    color.palette = c("+" = "azure3", "-" = "yellow3"),
+    )
+plt <- plt + ggtitle('AVA Alignment (100kbp bin identity)')
+
+# Write out the figure
+png(filename = out.png.name, width = 20, height = 10, res = 300, units = 'in')
+plt
 dev.off()
